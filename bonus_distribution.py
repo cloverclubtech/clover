4c7be63ca5dc7526c7e7440784f7d601cbc25ee2
Administrator Clover Club <administrator@cloverclub.tech>
2020-06-27 13:20:02 -0500

    Initial commit

---
+++ bonus_distribution.py
@@ -0,0 +1,43 @@
+import math
+
+
+def get_tree_distribution(clients, max_level, percentages):
+    """Get the balance distribution in the tree of a user"""
+    final_distribution = []
+    for client in clients:
+        if client['dist_val'] == 0:
+            continue
+        parents = []
+        if client['grouped_parents'] is not None:
+            for parent in client['grouped_parents'].split('|'):
+                data = parent.split(';')
+                parents.append({
+                    'parent_id': data[0],
+                    'max_level_dist_tree': int(data[1])
+                })
+        i = 0
+        while (i < max_level):
+            not_rounded_value = (
+                client['dist_val'] * percentages[i]['percentage']
+            ) / 100
+            comission_parent = math.floor(not_rounded_value * 100)/100.0
+            # Si no hay más padres en niveles superiores, todo va al fondo
+            # de comisiones no pagadas
+            if len(parents) <= i or i >= parents[i]['max_level_dist_tree']:
+                final_distribution.append(
+                    {
+                        'source_id': client['id'],
+                        'destiny_id': 0,
+                        'amount': comission_parent
+                    }
+                )
+            else:
+                final_distribution.append(
+                    {
+                        'source_id': client['id'],
+                        'destiny_id': parents[i]['parent_id'],
+                        'amount': comission_parent
+                    }
+                )
+            i += 1
+    return final_distribution
---
+++ get_winners.py
@@ -0,0 +1,86 @@
+"""Get winners"""
+
+
+class GetWinners:
+    """Get Winners."""
+
+    def removeDuplicateWinners(self, list):
+        """Remove duplicate winners."""
+        reply_winners = list[:]
+        for reply_winner in reply_winners:
+            num_repeated = 0
+            for winner in list:
+                if winner['user_id'] == reply_winner['user_id']:
+                    num_repeated += 1
+                    if num_repeated > 1:
+                        if winner['value_difference'] <= reply_winner['value_difference']:
+                            list.remove(reply_winner)
+                            break
+                        else:
+                            list.remove(winner)
+                            break
+        return list
+
+    def chooseWinners(self, bets, final_value, range):
+        """choose winners"""
+        winners = []
+        for bet in bets:
+            if final_value == bet['bet_value']:
+                winners.append(bet)
+
+        if len(winners) == 0:
+            validated_winners = []
+            aux_winners = []
+            validated_winners.append(final_value)
+            for bet in bets:
+                if bet['bet_value'] >= final_value - range and bet['bet_value'] <= final_value + range:
+                    bet['value_difference'] = abs(final_value - bet['bet_value'])
+                    winners.append(bet)
+
+            for winner in winners:
+                if validated_winners[-1] == winner['value_difference']:
+                    validated_winners.append(winner['value_difference'])
+                    aux_winners.append(winner)
+                elif winner['value_difference'] < validated_winners[-1]:
+                    validated_winners = []
+                    aux_winners = []
+                    validated_winners.append(winner['value_difference'])
+                    aux_winners.append(winner)
+
+            winners = self.removeDuplicateWinners(aux_winners)
+            closest_final_value = self.closestFinalValue(bets, winners, final_value, range)
+
+            result = {
+                'winners': winners,
+                'closest_final_value': closest_final_value,
+            }
+
+        return result
+
+    def closestFinalValue(self, bets, list_winners, final_value, range):
+        """closest final value"""
+        winners = []
+        list_aux = []
+        reply_winners = bets[:]
+        for bet in reply_winners:
+            for winner in list_winners:
+                if winner['user_id'] == bet['user_id']:
+                    bets.remove(bet)
+                    break
+
+        for bet in bets:
+            bet['value_difference'] = abs(final_value - bet['bet_value'])
+            winners.append(bet)
+        winners.sort(key=lambda x: x['value_difference'])
+
+        winners = self.removeDuplicateWinners(winners)
+
+        for winner in winners:
+            if len(list_aux) == 0:
+                list_aux.append([winner])
+            elif list_aux[-1][0]['value_difference'] == winner['value_difference']:
+                list_aux[-1].append(winner)
+            else:
+                list_aux.append([winner])
+
+        return list_aux
